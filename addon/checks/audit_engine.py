f8b16af04fa20c2910a2327cbaa6595067df95aa
Patrice Newson <sadejudah@yahoo.com>
2026-08-02 20:50:44 -0400

    Update audit_engine.py
    Connect collection validator to audit engine

--- addon/checks/audit_engine.py
+++ addon/checks/audit_engine.py
@@ -1,5 +1,6 @@
-from .naming_check import run_naming_check
 from .transform_check import run_transform_check
+from .naming_check import run_naming_check
+from .collection_check import run_collection_check
 
 
 def run_production_audit():
@@ -10,7 +11,10 @@ def run_production_audit():
     # Transform inspection
     all_results.extend(run_transform_check())
 
-    # Object naming inspection
+    # Naming inspection
     all_results.extend(run_naming_check())
+
+    # Collection inspection
+    all_results.extend(run_collection_check())
 
     return all_results
