7653896eca1626229730355e162277d3ccedf424
Patrice Newson <sadejudah@yahoo.com>
2026-08-04 18:01:58 -0400

    Create __init__.py for templates package
    Add __init__.py for character templates module

---
+++ addon/templates/__init__.py
@@ -0,0 +1,12 @@
+"""
+Character Templates
+
+Provides predefined production templates used by the
+Blender Production System.
+"""
+
+from .generic import TEMPLATE as GENERIC_TEMPLATE
+
+__all__ = (
+    "GENERIC_TEMPLATE",
+)
