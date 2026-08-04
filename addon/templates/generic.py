94e4703ef0379ca335c6483dca4dd7e886231a9a
Patrice Newson <sadejudah@yahoo.com>
2026-08-04 18:03:12 -0400

    Create generic character template in generic.py
    Added a generic character template for the Blender Production System.

---
+++ addon/templates/generic.py
@@ -0,0 +1,33 @@
+"""
+Generic Character Template
+
+Default template used by the Blender Production System.
+"""
+
+TEMPLATE = {
+
+    "name": "Generic",
+
+    "species": "Generic",
+
+    "height": 1.00,
+
+    "blueprint_preset": "STANDARD",
+
+    "head_style": "standard",
+
+    "torso_style": "standard",
+
+    "arm_style": "standard",
+
+    "leg_style": "standard",
+
+    "hand_style": "standard",
+
+    "foot_style": "standard",
+
+    "rig_template": None,
+
+    "material_template": None,
+
+}
