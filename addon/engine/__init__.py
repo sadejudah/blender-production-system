091bd54ce1a7e461c9b9145d51229a6ab77d7551
Patrice Newson <sadejudah@yahoo.com>
2026-08-04 21:41:08 -0400

    Add BuildContext and create_build_context to exports

--- addon/engine/__init__.py
+++ addon/engine/__init__.py
@@ -1,1 +1,9 @@
+"""Generator engine package."""
 
+from .build_context import BuildContext
+from .build_context import create_build_context
+
+__all__ = (
+    "BuildContext",
+    "create_build_context",
+)
