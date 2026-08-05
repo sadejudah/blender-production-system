155919185352c61381cf0e6e95d3ee062e3122a9
Patrice Newson <sadejudah@yahoo.com>
2026-08-04 21:43:16 -0400

    Update module docstring and imports

--- addon/engine/__init__.py
+++ addon/engine/__init__.py
@@ -1,9 +1,13 @@
-"""Generator engine package."""
+"""Blender Production System Engine."""
 
 from .build_context import BuildContext
 from .build_context import create_build_context
+from .build_character import build_character
+from .generator_engine import GeneratorEngine
 
 __all__ = (
     "BuildContext",
+    "GeneratorEngine",
+    "build_character",
     "create_build_context",
 )
