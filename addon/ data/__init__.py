eec6f5ff6336a5eb0cd0da3cdc9dfdd97ecdc2cf
Patrice Newson <sadejudah@yahoo.com>
2026-08-04 21:07:45 -0400

    Add character data imports and define __all__

--- addon/ data/__init__.py
+++ addon/ data/__init__.py
@@ -1,1 +1,10 @@
+"""Shared production data for the Blender Production System."""
 
+from .character_data import CharacterData
+from .character_data import resolve_character_data
+
+
+__all__ = (
+    "CharacterData",
+    "resolve_character_data",
+)
